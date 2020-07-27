ec7e566a35024446e0804aacf87651a8446317fe
Arturo Moncada-Torres <arturomoncadatorres@gmail.com>
2020-07-27 19:48:28 +0200

    Updated requirements (attempt to fix docs in Read the Docs)

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ with open("README.md") as f:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['pygments>=2.5.1', ]
+requirements = ['pygments>=2.5.1', 
+                'numpy>=1.14.0',
+                'h5py>=2.10.0']
 
 setup_requirements = ['pytest-runner', ]
 
